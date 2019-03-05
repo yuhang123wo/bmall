9a3653b760a59b67a223a1579695c43867ef64ce
yuhang <yuhang@stargoto.com>
2019-03-05 17:35:43 +0800

    项目初始化

---
+++ generator/src/main/java/generator/CodeGenerator.java
@@ -0,0 +1,118 @@
+package generator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.FileOutConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.TemplateConfig;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+public class CodeGenerator {
+
+	private static final String model = "mall";
+	private static final String JDBC_URL = "jdbc:mysql://106.12.204.95:3306/mall?useUnicode=true&useSSL=false&characterEncoding=utf8";
+	private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	private static final String JDBC_USERNAME = "root";
+	private static final String JDBC_PWD = "yuhang123!wo";
+	private static final String PATH = "D://autoCode/mall";
+
+	public static void main(String[] args) {
+		// 代码生成器
+		AutoGenerator mpg = new AutoGenerator();
+
+		// 全局配置
+		GlobalConfig gc = new GlobalConfig();
+//		String projectPath = System.getProperty("user.dir");
+		gc.setOutputDir(PATH);
+		gc.setAuthor("yuhang");
+		gc.setOpen(false);
+		gc.setSwagger2(true);// swagger2配置
+		gc.setFileOverride(true);// 是否覆盖已有文件
+		gc.setDateType(DateType.ONLY_DATE);
+		gc.setEntityName("%s");
+		mpg.setGlobalConfig(gc);
+
+		// 数据源配置
+		DataSourceConfig dsc = new DataSourceConfig();
+		dsc.setUrl(JDBC_URL);
+		// dsc.setSchemaName("public");
+		dsc.setDriverName(JDBC_DRIVER);
+		dsc.setUsername(JDBC_USERNAME);
+		dsc.setPassword(JDBC_PWD);
+		mpg.setDataSource(dsc);
+		// 包配置
+		PackageConfig pc = new PackageConfig();
+		pc.setModuleName(model);
+		pc.setParent("cn.yh.st");
+		mpg.setPackageInfo(pc);
+
+		// 自定义配置
+		InjectionConfig cfg = new InjectionConfig() {
+			@Override
+			public void initMap() {
+				// to do nothing
+			}
+		};
+
+		// 如果模板引擎是 freemarker
+		String templatePath = "/templates/mapper.xml.ftl";
+		// 如果模板引擎是 velocity
+		// String templatePath = "/templates/mapper.xml.vm";
+
+		// 自定义输出配置
+		List<FileOutConfig> focList = new ArrayList<>();
+		// 自定义配置会被优先输出
+		focList.add(new FileOutConfig(templatePath) {
+			@Override
+			public String outputFile(TableInfo tableInfo) {
+				// 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+				return PATH + "/mapper/" + pc.getModuleName() + "/" + tableInfo.getEntityName() + "Mapper"
+						+ StringPool.DOT_XML;
+			}
+		});
+
+		cfg.setFileOutConfigList(focList);
+		mpg.setCfg(cfg);
+
+		// 配置模板
+		TemplateConfig templateConfig = new TemplateConfig();
+
+		// 配置自定义输出模板
+		// 指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+		// templateConfig.setEntity("templates/entity2.java");
+		// templateConfig.setService();
+		// templateConfig.setController();
+
+		templateConfig.setXml(null);
+		mpg.setTemplate(templateConfig);
+
+		// 策略配置
+		StrategyConfig strategy = new StrategyConfig();
+		strategy.setNaming(NamingStrategy.underline_to_camel);
+		strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+		strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
+		strategy.setRestControllerStyle(true);
+		strategy.setSuperControllerClass("com.baomidou.ant.common.BaseController");
+//		strategy.setInclude("test");
+		strategy.setSuperEntityColumns("id");
+		strategy.setControllerMappingHyphenStyle(true);
+		strategy.setTablePrefix(pc.getModuleName() + "_");
+		mpg.setStrategy(strategy);
+		mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+		mpg.execute();
+	}
+}
